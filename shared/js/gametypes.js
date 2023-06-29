21ea88dab400244ecc56f56cc32c980e4e2b1fea
loopwormsbot <balkshamster@proton.me>
2023-06-29 16:55:00 +0000

    add LW | Mummy, 0xddff9aa18c3bbe0db70fde53e9472d08dba2ed64b24e266735a89656fec279be, armor, bot_add_nfts_2023_06_29T16_54_55

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -388,7 +388,8 @@ Types = {
         NFT_950548ea793d3c955053b78d0714833b57b5c61c4b438e4c04c51d9819b9e3e4: 359,
         NFT_c22cb7bee6da9d2563cafa0387bca094e6c791d5e8b24de2b3937c6678d2e4df: 360,
         NFT_c98876ba1f9a38bc8699a36d28f804141b33085a59afd02c2bf911ba13afa92a: 361,
-        NFT_d60dd36648bde090980e0073f21eb3dbfa38fbe4c452a0e6f8eaf6c92feeb527: 362 // @lastid@
+        NFT_d60dd36648bde090980e0073f21eb3dbfa38fbe4c452a0e6f8eaf6c92feeb527: 362,
+        NFT_ddff9aa18c3bbe0db70fde53e9472d08dba2ed64b24e266735a89656fec279be: 363 // @lastid@
         //@nextIdLine@
     },
     
@@ -753,6 +754,7 @@ var kinds = {
     NFT_c22cb7bee6da9d2563cafa0387bca094e6c791d5e8b24de2b3937c6678d2e4df: [Types.Entities.NFT_c22cb7bee6da9d2563cafa0387bca094e6c791d5e8b24de2b3937c6678d2e4df, "armor"],
     NFT_c98876ba1f9a38bc8699a36d28f804141b33085a59afd02c2bf911ba13afa92a: [Types.Entities.NFT_c98876ba1f9a38bc8699a36d28f804141b33085a59afd02c2bf911ba13afa92a, "armor"],
     NFT_d60dd36648bde090980e0073f21eb3dbfa38fbe4c452a0e6f8eaf6c92feeb527: [Types.Entities.NFT_d60dd36648bde090980e0073f21eb3dbfa38fbe4c452a0e6f8eaf6c92feeb527, "armor"],
+    NFT_ddff9aa18c3bbe0db70fde53e9472d08dba2ed64b24e266735a89656fec279be: [Types.Entities.NFT_ddff9aa18c3bbe0db70fde53e9472d08dba2ed64b24e266735a89656fec279be, "armor"],
     // @nextSpriteLine@
 
     
@@ -1362,6 +1364,8 @@ Types.getArmorRank = function(armorKind) {
             return 1;
         case Types.Entities.NFT_d60dd36648bde090980e0073f21eb3dbfa38fbe4c452a0e6f8eaf6c92feeb527:
             return 0;
+        case Types.Entities.NFT_ddff9aa18c3bbe0db70fde53e9472d08dba2ed64b24e266735a89656fec279be:
+            return 1;
         // @nextarmorrank@
         default:
             console.error("Unknown armor rank");
